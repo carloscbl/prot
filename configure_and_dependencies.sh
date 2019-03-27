5c316dbd8c699bb51c2e931c8ea2aa6f5b9af1ec
cbernal <cbernal@allot.com>
2019-03-27 16:25:49 +0100

    [Added script for initialize the project correctly and install boost]

---
+++ configure_and_dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+DIR2=$(pwd)
+
+
+sh ./dependencies.sh
+
+cd ${DIR2}
+mkdir build
+cd build
+cmake ..
+make
---
+++ dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd thirdparty
+wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz
+tar -xvf boost_1_69_0.tar.gz
+cd boost_1_69_0
+./bootstrap.sh --prefix=/usr/
+./b2 -j 3
+sudo ./b2 -j 3 install
