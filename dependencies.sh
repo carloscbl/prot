f93a58ef905ccd969db0583d67c8bbae474bc602
carlos <cbernal@allot.com>
2019-05-07 18:49:52 +0200

    Finished the definition file for the parser of forms

--- dependencies.sh
+++ dependencies.sh
@@ -4,5 +4,5 @@ wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz
 tar -xvf boost_1_69_0.tar.gz
 cd boost_1_69_0
 ./bootstrap.sh --prefix=/usr/
-./b2 -j 3
-sudo ./b2 -j 3 install
+./b2 -j 7 
+sudo ./b2 -j 7 install
