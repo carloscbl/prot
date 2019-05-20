b1699817a5e4afcb80c0c459569535f19f3ce8d0
carlos <cbernal@allot.com>
2019-05-20 13:17:37 +0200

    removed unnecesary dependencies

--- configure_and_dependencies.sh
+++ configure_and_dependencies.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 DIR2=$(pwd)
-sudo add-apt-repository ppa:jonathonf/gcc-9.0
-sudo apt-get install gcc-9
-
-#sudo update-alternatives --remove-all gcc 
-#sudo update-alternatives --remove-all g++
-
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 900 --slave /usr/bin/g++ g++ /usr/bin/g++-9
 
 sh ./dependencies.sh
 
