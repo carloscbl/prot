dd790ad7043d571deba8ac9893387a310768ae5d
carloscbl <carloscbl@hotmail.com>
2019-05-28 23:20:00 +0200

    added instances

--- source/form_parser.cpp
+++ source/form_parser.cpp
@@ -3,9 +3,9 @@
 form_parser::form_parser(const json & j):j(j){
 
     subsections = map<string,unique_ptr<form_subsection_ADT>>();
-    subsections["form"] = nullptr;
-    subsections["expressions"] = nullptr;
-    subsections["variables"] = nullptr;
-    subsections["configurables"] = nullptr;
-    subsections["questions"] = nullptr;
+    subsections["form"] = make_unique<form_subsection_ADT>(j,"form");
+    subsections["expressions"] = make_unique<form_subsection_ADT>(j,"expressions");
+    subsections["variables"] = make_unique<form_subsection_ADT>(j,"variables");
+    subsections["configurables"] = make_unique<form_subsection_ADT>(j,"configurables");
+    subsections["questions"] = make_unique<form_subsection_ADT>(j,"questions");
 }
