ffd579a2f1cb7249bee0a6fa791a7ba50bdb30d3
carloscbl <carloscbl@hotmail.com>
2019-07-28 19:33:48 +0200

    Changed the handling of coneversion

--- source/type_container.h
+++ source/type_container.h
@@ -2,17 +2,18 @@
 #define TYPE_CONTAINER_H
 #include <string>
 #include <functional>
+#include <cstdlib>
 #include <map>
 #include <any>
 
 
 using namespace std;
 
-std::map<string,function<any(string)>> conversors_map{
-    {"INTEGER", (function<int(string)>)[](string s) -> int{ return std::stoi(s);}},
-    {"FLOAT", (function<float(string)>)[](string s)->float{ return std::stof(s);}},
-    {"BOOL", (function<bool(string)>)[](string s)->bool{ return (bool)std::stoi(s);}},
-    {"STRING", (function<string(string)>)[](string s)->string{ return s;}},
+std::map<string,function<any(string,char *)>> conversors_map{
+    {"INTEGER", (function<int(string,char *)>)[](string s, char * p) -> int{ return stoi(s, &p, 10);}},
+    {"FLOAT", (function<float(string,char *)>)[](string s)->float{ return std::stof(s);}},
+    {"BOOL", (function<bool(string,char *)>)[](string s)->bool{ return (bool)std::stoi(s);}},
+    {"STRING", (function<string(string,char *)>)[](string s)->string{ return s;}},
 };
 
 
