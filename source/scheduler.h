74433a69fe9e4ba9b8adf9adebb8cbe85a37aa0f
carloscbl <carloscbl@hotmail.com>
2019-08-01 23:24:22 +0200

    Added strong type to get validated task agains scheduler

--- source/scheduler.h
+++ source/scheduler.h
@@ -16,13 +16,28 @@ typedef boost::icl::interval_map<time_t, itasker> im_t;
 // a time interval
 typedef boost::icl::interval<time_t> interval_t;
 
+class scheduled_validated_task{
+private:
+    shared_ptr<itasker> task;
+    bool validate(){
+        
+    };
+public:
+    bool is_valid(){ return validate(); }
+}
+
 class scheduler : public ischeduler
 {
 private:
-    /* data */
+    im_t m_interval_map;
+    bool add_task(itasker * task);
 public:
     scheduler(/* args */);
     ~scheduler();
+    vector<itasker*> get_tasks_in(interval_t interval);
+    bool get_range(time_t start,time_t end) override;
+    bool find_range(time_t start,time_t end, time_t min_dur) override;
+    bool find_relative(scheduled item, time_with_negative after_before, time_t end, time_t min_dur) override;
 };
 
 scheduler::scheduler(/* args */)
@@ -31,6 +46,11 @@ scheduler::scheduler(/* args */)
 
 scheduler::~scheduler()
 {
+}
+
+bool scheduler::find_relative(scheduled item, time_with_negative after_before, time_t end, time_t min_dur) 
+{
+    return false;
 }
 
 #endif //SCHEDULER_H
