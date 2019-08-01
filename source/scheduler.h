d8afa4a7ca29ceab8b9a4a41ff02b96382415661
carloscbl <carloscbl@hotmail.com>
2019-08-01 16:50:38 +0200

    added dependences on interval_map

--- source/scheduler.h
+++ source/scheduler.h
@@ -2,6 +2,19 @@
 #define SCHEDULER_H
 
 #include "ischeduler.h"
+#include <map>
+#include <set>
+#include <climits>
+#include "itasker.h"
+
+#include <boost/icl/interval.hpp>
+#include <boost/icl/interval_map.hpp>
+
+// Set of IDs that cover a time interval
+// interval tree from intervals of timestamps to a set of ids
+typedef boost::icl::interval_map<time_t, itasker> im_t;
+// a time interval
+typedef boost::icl::interval<time_t> interval_t;
 
 class scheduler : public ischeduler
 {
