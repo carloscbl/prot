17dac7cc61f5027c383e7efe7657b6d1c4ec34be
carloscbl <carloscbl@hotmail.com>
2019-09-19 22:34:42 +0200

    Fixed optional params, now time to remove the old obtention of tasks

--- source/task.cpp
+++ source/task.cpp
@@ -23,15 +23,17 @@ void task_space::to_json(nlohmann::json& j, const task_space::task& p) {
 
 
 void task_space::from_json(const nlohmann::json& j, task_space::task& p){
-        //j.at("name").get_to(p.name);
-        p.m_priority.set_priority( j.at("priority").get<std::string>() );
-        j.at("description").get_to(p.description);
-        j.at("duration").get_to(p.m_duration);
+    //j.at("name").get_to(p.name);
+    p.m_priority.set_priority( j.at("priority").get<std::string>() );
+    j.at("description").get_to(p.description);
+    j.at("duration").get_to(p.m_duration);
+    //Optional
+    if(j.find("restrictions") != j.end()){
+        p.m_restrictions.set_restrictions( j.at("restrictions") .get<std::set<std::string>>() );
+    }
+    if(j.find("when") != j.end()){
         j.at("when").get_to(p.m_when);
-        //Optional
-        if(j.find("restrictions") != j.end()){
-            p.m_restrictions.set_restrictions( j.at("restrictions") .get<std::set<std::string>>() );
-        }
+    }
 }
 
 task_space::task::task() : CRUD_actionable(this->tasks_map, setters),dateUTC(time(nullptr))
