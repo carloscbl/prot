7a75084474d407ba06b497e3647ba74890a43627
carloscbl <carloscbl@hotmail.com>
2019-11-06 21:52:07 +0100

    added base methods for persistence

--- source/persistor.cpp
+++ source/persistor.cpp
@@ -4,10 +4,16 @@ persistor & persistor::get_persistor_instance(){
     if(!persistor_instance) throw std::runtime_error("not initialized yet?");
     return *persistor_instance;
 }
-persistor::persistor(/* args */)
+
+persistor::persistor(string route_table_or_folder)
 {
 }
 
 persistor::~persistor()
 {
+}
+
+persistor & persistor::set_path(string route_table_or_folder){
+    this->path = route_table_or_folder;
+    return *this;
 }
--- source/persistor.h
+++ source/persistor.h
@@ -2,6 +2,11 @@
 #define PERSISTOR_H
 
 #include <memory>
+#include "json.hpp"
+
+using json = nlohmann::json;
+using std::string;
+
 /*design
     this class is intended to be an abstraction of serialization to json,
     regardless the configuration to persist on disk or network db like mongodb
@@ -14,10 +19,14 @@ class persistor
 private:
     /* data */
     static unique_ptr<persistor> persistor_instance;
+protected:
+    string path;
 public:
-    persistor(/* args */);
+    persistor(string route_table_or_folder);
     ~persistor();
     static persistor & get_persistor_instance();
+    virtual void save (string index_name, json file) noexcept = 0;
+    persistor & set_path(string route_table_or_folder);
 };
 
 
