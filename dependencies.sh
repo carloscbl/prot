8be5e95719ca417f63547145c49eace899d8ed53
carloscbl <carloscbl@hotmail.com>
2019-11-17 00:45:00 +0100

    Added automation for install deps of the project

--- dependencies.sh
+++ dependencies.sh
@@ -2,17 +2,39 @@
 sudo snap install cmake --classic
 sudo apt install -y gcc-9 g++-9 make wget tar libmariadbclient-dev
 cd thirdparty
-wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz
-tar -xvf boost_1_69_0.tar.gz
+# wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz
+# tar -xvf boost_1_69_0.tar.gz
 cd boost_1_69_0
-sudo ./bootstrap.sh 
-#sudo ./bootstrap.sh --with-python=/usr/local/bin/python3 --with-python-version=3.7 --with-python-root=/usr/local/lib/python3.7 --prefix=/usr/
-sudo ./b2 clean
-sudo ./b2 --enable-unicode=ucs4 -j7 install
+# sudo ./bootstrap.sh 
+# #sudo ./bootstrap.sh --with-python=/usr/local/bin/python3 --with-python-version=3.7 --with-python-root=/usr/local/lib/python3.7 --prefix=/usr/
+# sudo ./b2 clean
+# sudo ./b2 --enable-unicode=ucs4 -j7 install
+cd ..
 
-#For build mongocxx driver
-# cd ..
-# git clone https://github.com/mongodb/mongo-cxx-driver.git     --branch releases/stable --depth 1
-# cd mongo-cxx-driver/build/
-# cmake -DCMAKE_BUILD_TYPE=Release -DBSONCXX_POLY_USE_BOOST=1     -DCMAKE_INSTALL_PREFIX=/usr/local ..
-# sudo make && sudo make install
+
+#date
+cd HowardHinnantDate/date
+mkdir build
+cd build
+cmake ..
+make -j 5
+sudo make install
+cd ../../..
+
+#sqlpp11
+cd sql/sqlpp11
+mkdir build
+cd build
+cmake ..
+make -j 5
+sudo make install
+cd ../../..
+
+#connector
+cd sql/sqlpp11-connector-mysql
+mkdir build
+cd build
+cmake -DUSE_MARIADB=1 - -DDATE_INCLUDE_DIR=/usr/local/include/ ..
+make -j 5
+sudo make install
+cd ../../..
