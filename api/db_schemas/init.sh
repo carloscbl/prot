7044de5ce9d8221666694b95b95efa6cbeec49c9
irala <nineirm@gmail.com>
2020-01-13 20:13:51 +0100

    fixed db order

--- api/db_schemas/init.sh
+++ api/db_schemas/init.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
-find /docker-entrypoint-initdb.d -mindepth 2 -type f -print0 | while read -d $'\0' f; do
-    echo $f
-  case "$f" in
-    *.sh)
-      if [ -x "$f" ]; then
-        echo "$0: running $f"
-        "$f"
-      else
-        echo "$0: sourcing $f"
-        . "$f"
-      fi
-      ;;
-    *.sql)    echo "$0: running $f"; "${mysql[@]}" < "$f"; echo ;;
-    *.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${mysql[@]}"; echo ;;
-    *)        echo "$0: ignoring $f" ;;
-  esac
-  echo
-done
+${mysql[@]} < /docker-entrypoint-initdb.d/prot/prot.sql
+${mysql[@]} < /docker-entrypoint-initdb.d/services_prot/bprot.sql
+# find /docker-entrypoint-initdb.d -mindepth 1 -type f -print0 | while read -d $'\0' f; do
+#     echo $f
+#   case "$f" in
+#     *.sh)
+#       if [ -x "$f" ]; then
+#         echo "$0: running $f"
+#         "$f"
+#       else
+#         echo "$0: sourcing $f"
+#         . "$f"
+#       fi
+#       ;;
+#     *.sql)    echo "$0: running $f"; "${mysql[@]}" < "$f"; echo ;;
+#     *.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${mysql[@]}"; echo ;;
+#     *)        echo "$0: ignoring $f" ;;
+#   esac
+#   echo
+# done
