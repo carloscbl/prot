83fd4a0fff3ac8341c821e871918d8c3093a1019
Carlos Bernal <cbernal@allot.com>
2020-07-30 14:16:44 +0200

    added check of wildcard

--- source/expanded_taskstory_t.h
+++ source/expanded_taskstory_t.h
@@ -1,8 +1,10 @@
 #ifndef EXPANDED_TASKSTORY_T_H
 #define EXPANDED_TASKSTORY_T_H
 #include "json.hpp"
+#include <optional>
 #include "app_parser.h"
-
+using std::optional;
+using std::nullopt;
 using nlohmann::json;
 
 class expand_taskstory_t {
@@ -18,8 +20,23 @@ public:
 
 unique_ptr<json> expand_taskstory_t::exapand_matrix(const json & type_details){
     for(auto [k,v] : m_nqdati.raw_taskstory.items()){
-        
+        if (!is_wildcard(v)){ continue; }
+
+        for(auto [k,v] : m_nqdati.type_details[""].items()){
+
+
+
     }
+}
+
+bool is_wildcard(json raw_task){ // Returns the wil
+    auto wild_task = raw_task.find("wildcard_task");
+    if(wild_task == raw_task.end()) return false;
+
+    auto enabled = wild_task.value().find("enabled");
+    if(enabled == wild_task.value().end() && enabled.value().get<bool>() == false) return false;
+
+    return true;
 }
 
 #endif //EXPANDED_TASKSTORY_T_H
